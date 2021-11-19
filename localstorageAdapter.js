d1d951f1f619fc2e1b40280bee347d6d48a36e70
Vitaly Dayneko <VitalyDayneko@coherentsolutions.com>
2021-11-19 12:48:40 +0300

    add localStorage adapter

---
+++ localstorageAdapter.js
@@ -0,0 +1,41 @@
+class LocalStorageAdapter {
+  constructor(keyName, dataType) {
+    this.keyName = keyName;
+    this.dataType = dataType;
+  }
+
+  getValue() {
+    const value = localStorage.getItem(this.keyName);
+    const dataTypeMap = {
+      array: [],
+      object: {},
+      string: '',
+      boolean: false,
+    };
+
+    return (value !== null) ? JSON.parse(value) : dataTypeMap[this.dataType];
+  };
+
+  setValue(data) {
+    const prevValue = this.getValue();
+    const newValueFromTypeMap = {
+      array: () => [...prevValue, data],
+      object: () => ({...prevValue, ...data}),
+      string: () => String(data),
+      boolean: () => Boolean(data),
+    };
+
+    const newValue = newValueFromTypeMap[this.dataType]();
+
+    localStorage.setItem(this.keyName, JSON.stringify(newValue));
+  };
+
+  removeKey() {
+    localStorage.removeItem(this.keyName);
+  };
+};
+
+
+/* const testArray = new LocalStorageAdapter('testArray', 'array');
+const testString = new LocalStorageAdapter('testString', 'string');
+const testObject = new LocalStorageAdapter('testObject', 'object'); */
